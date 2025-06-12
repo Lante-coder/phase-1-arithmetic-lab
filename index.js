fb404812ea9e21def15525d4e73f8dac8a995273
Lante_coder <allanmwangi816@gmail.com>
2025-06-12 17:50:17 +0300

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 31
+const num2 = 2
+let multiply = num1*num2
+
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 =7
+const num4 =32
+
+const mod = 32 % 7
+
+
+const max = Math.max(1, 5, 10, 15, 20);
 
 // Write your code here
